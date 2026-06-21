261dbc5acad2d9d45773ab27de02da33ee9abbdd
manoranjith46 <manoranjithd46@gmail.com>
2026-06-21 23:51:21 +0530

    Update API base URL to Cloud Run production

--- client/lib/config/api_config.dart
+++ client/lib/config/api_config.dart
@@ -4,7 +4,14 @@ import 'package:flutter/foundation.dart';
 class ApiConfig {
   ApiConfig._();
 
+  // Set this to false if you want to test against the local server (localhost)
+  static bool useProduction = true;
+
   static String get baseUrl {
+    if (useProduction) {
+      return 'https://carbon-lens-server-664585468768.asia-south1.run.app';
+    }
+
     if (kIsWeb) {
       return 'http://localhost:5000';
     }
